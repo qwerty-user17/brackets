cfd1bc6e412907f7067637fc4cb83aefaf139b3a
qwerty-user17 <balabandanil90@gmail.com>
2025-08-29 17:20:03 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const stack = [];
+  const openBrackets = {};
+  const closeBrackets = {};
+
+  bracketsConfig.forEach(([open, close]) => {
+    openBrackets[open] = close;
+    closeBrackets[close] = open;
+  });
+
+  const isValid = str.split('').every((char) => {
+    const isOpen = Object.prototype.hasOwnProperty.call(openBrackets, char);
+    const isClose = Object.prototype.hasOwnProperty.call(closeBrackets, char);
+
+    if (isOpen && isClose && openBrackets[char] === char) {
+      if (stack[stack.length - 1] === char) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+      return true;
+    }
+
+    if (isOpen) {
+      stack.push(char);
+      return true;
+    }
+
+    if (isClose) {
+      if (stack.length === 0) return false;
+      const last = stack.pop();
+      return last === closeBrackets[char];
+    }
+
+    return true;
+  });
+
+  return isValid && stack.length === 0;
 };
